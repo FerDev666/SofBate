b64ce039f1781adbebd7eef848a33f527ca0b92d
Fernando Cruz Banegas <nano.fer777@gmail.com>
2019-11-01 18:22:22 -0400

    add all libraries js

--- resources/js/components/GridComponent.vue
+++ resources/js/components/GridComponent.vue
@@ -1,29 +1,30 @@
 <template>
     <div>
         <kendo-datasource ref="datasource1"
-                      :type="'odata'"
-                      :transport-read-url="'https://demos.telerik.com/kendo-ui/service/Northwind.svc/Orders'"
-                      :page-size='20'
-                      :server-paging="true"
-                      :server-filtering="true"
-                      :schema-model-fields="dsSchemaFields">
-    </kendo-datasource>
+            :schema-total="'total'"
+            :schema-data="'data'"
+            :transport-read-url="'/api/products'"
+            :page-size='5'
+            :server-paging="true"
+            :server-filtering="true"
+            :schema-model-fields="dsSchemaFields">
+        </kendo-datasource>
 
-    <kendo-grid :data-source-ref="'datasource1'"
+        <kendo-grid :data-source-ref="'datasource1'"
                 :pageable="true"
                 :filterable-mode="'row'">
-        <kendo-grid-column :field="'OrderID'"
-                           :width="180"
+        <kendo-grid-column :field="'id'"
+                           :width="50"
                            :filterable-cell-show-operators="false"></kendo-grid-column>
-        <kendo-grid-column :field="'ShipName'"
+        <kendo-grid-column :field="'title'"
                            :title="'Ship Name'"
                            :width="240"
                            :filterable-cell-operator="'contains'"
                            :filterable-cell-suggestion-operator="'contains'"></kendo-grid-column>
-        <kendo-grid-column :field="'Freight'"
+        <kendo-grid-column :field="'category'"
                            :width="180"
                            :filterable-cell-operator="'gte'"></kendo-grid-column>
-        <kendo-grid-column :field="'OrderDate'"
+        <kendo-grid-column :field="'created_at'"
                            :title="'Order Date'"
                            :format="'{0:MM/dd/yyyy}'"
                            :width="180"></kendo-grid-column>
@@ -36,13 +37,22 @@
         data() {
             return {
                 dsSchemaFields: {
-                    OrderID: { type: "number" },
-                    Freight: { type: "number" },
-                    ShipName: { type: "string" },
-                    OrderDate: { type: "date" },
-                    ShipCity: { type: "string" }
+                    id: { type: "number" },
+                    title: { type: "string" },
+                    description: { type: "string" },
+                    created_at: { type: "date" },
+                    category: { type: "string" }
                 }
             }
         }
     }
 </script>
+<style scoped>
+    .k-grid {
+        font-size: 12px;
+    }
+
+    .k-grid td {
+    line-height: 1em;
+}
+</style>
